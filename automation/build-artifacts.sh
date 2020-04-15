cebb32c0170ceddbbea976e8f5c518c71c8a0078
Sandro Bonazzola <sbonazzo@redhat.com>
2020-04-15 17:28:23 +0200

    prepare for next development iteration
    Change-Id: If0a2c5919d31e875fd7d5a98f0de241a70c50009
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/build-artifacts.sh
+++ automation/build-artifacts.sh
@@ -4,8 +4,8 @@ rm -rf exported-artifacts
 mkdir -p exported-artifacts
 git_head=$(git log -1 --pretty=format:%h)
 GIT_RELEASE=$(date --utc +%Y%m%d).git${git_head}
-make  spec
+make "rpmsuffix=.${GIT_RELEASE}" spec
 yum-builddep -y ovirt-jboss-modules-maven-plugin.spec
-make rpm
+make "rpmsuffix=.${GIT_RELEASE}" rpm
 find rpmtop/RPMS rpmtop/SRPMS -name '*rpm' -exec mv {} exported-artifacts \;
 mv *.tar.gz exported-artifacts/
