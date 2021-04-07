e5a060e1f22437410a536135bbe000db04420e2d
Martin Perina <mperina@redhat.com>
2021-04-07 12:32:23 +0200

    Allow oVirt dependencies when testing created RPMs
    Change-Id: I5ad9c0100d90fd5bd01bb4e643ac30c4b49c4653
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- automation/check-merged.sh
+++ automation/check-merged.sh
@@ -4,6 +4,21 @@ echo "#######################################################################"
 echo "# Installation tests"
 "${0%/*}"/build-artifacts.sh
 shopt -s extglob
-dnf install -y exported-artifacts/!(*src).rpm
+pushd exported-artifacts
+    rm -f /etc/yum.conf
+    dnf reinstall -y system-release dnf
+    [[ -d /etc/dnf ]] && [[ -x /usr/bin/dnf ]] && dnf -y reinstall dnf-conf
+    [[ -d /etc/dnf ]] && sed -i -re 's#^(reposdir *= *).*$#\1/etc/yum.repos.d#' '/etc/dnf/dnf.conf'
+    [[ -e /etc/dnf/dnf.conf ]] && echo "deltarpm=False" >> /etc/dnf/dnf.conf
+    rm -f /etc/yum/yum.conf
+
+    dnf repolist enabled
+    dnf clean all
+    dnf install -y http://resources.ovirt.org/pub/yum-repo/ovirt-release-master.rpm
+
+    dnf module enable pki-deps javapackages-tools
+    dnf --downloadonly install *noarch.rpm
+
+popd
 echo "#######################################################################"
 
